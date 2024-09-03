bcaa92f675b97f71510ba31aa064580e5e46417b
Álvaro <62889807+alvarogfn@users.noreply.github.com>
2024-09-03 19:52:31 -0300

    feat(server): sort votingSystem

--- packages/server/src/game/services/voting-system.service.ts
+++ packages/server/src/game/services/voting-system.service.ts
@@ -1,34 +1,37 @@
-import VotingSystemEntity, { VotingSystemDocument } from "@/game/entities/voting-system.entity";
-import { VotingSystem } from "@/game/models/voting-system.model";
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import VotingSystemEntity, {VotingSystemDocument} from "@/game/entities/voting-system.entity";
+import {VotingSystem} from "@/game/models/voting-system.model";
+import {Injectable} from "@nestjs/common";
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
 
 @Injectable()
 export class VotingSystemService {
-	constructor(@InjectModel(VotingSystemEntity.name) private votingSystemModel: Model<VotingSystemDocument>) {}
+  constructor(@InjectModel(VotingSystemEntity.name) private votingSystemModel: Model<VotingSystemDocument>) {
+  }
 
-	public async findById(id: string): Promise<VotingSystem> {
-		return this.votingSystemModel.findById(id);
-	}
+  public async findById(id: string): Promise<DeepPartial<VotingSystem>> {
+    return this.votingSystemModel.findById(id).sort({cards: 'asc'});
+  }
 
-	public async findAll(search?: string): Promise<VotingSystem[]> {
-		let votingSystems: VotingSystemDocument[];
+  public async findAll(search?: string): Promise<VotingSystem[]> {
+    let votingSystems: VotingSystemDocument[];
 
-		if (search) {
-			votingSystems = await this.votingSystemModel
-				.find({
-					search: { $regex: search, $options: "i" },
-				})
-				.exec();
-		} else {
-			votingSystems = await this.votingSystemModel.find().exec();
-		}
+    const sanitizedSearch = search?.replace(/[^\w\s]/gi, '').replace(" ", '.+');
 
-		return votingSystems.map((votingSystem) => ({
-			cards: votingSystem.cards,
-			id: votingSystem._id.toString(),
-			name: votingSystem.name,
-		}));
-	}
+    if (sanitizedSearch) {
+      this.votingSystemModel
+          .find({
+            search: {$regex: sanitizedSearch, $options: "i"},
+          })
+          .exec();
+    } else {
+      votingSystems = await this.votingSystemModel.find().exec();
+    }
+
+    return votingSystems.map((votingSystem) => ({
+      cards: votingSystem.cards,
+      id: votingSystem._id.toString(),
+      name: votingSystem.name,
+    }));
+  }
 }
